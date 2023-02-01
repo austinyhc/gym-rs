e9b68bb1754e125699a2b4f27a64f0a2b69463f0
Austin Chen <austin.yhc@gmail.com>
2023-02-01 17:57:33 +0800

    🏗️ Add Clone trait to struct linked_list

--- src/utils/linked_list.rs
+++ src/utils/linked_list.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
@@ -11,13 +11,12 @@ impl ListNode {
     }
 }
 
-// helper function for test
 pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
     let mut current = None;
-    for &v in vec.iter().rev() {
-        let mut node = ListNode::new(v);
+    for &num in vec.iter().rev() {
+        let mut node = ListNode::new(num);
         node.next = current;
-        current = Some(Box::new(node));
+        current = Some(Box::<ListNode>::new(node));
     }
     current
 }
